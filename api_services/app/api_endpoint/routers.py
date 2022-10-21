1826c6e4ca415633dc75d7910e4dd4f98acf732d
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2022-10-21 19:57:31 +0700

    Additional routers for face detection

--- api_services/app/api_endpoint/routers.py
+++ api_services/app/api_endpoint/routers.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from api_endpoint.resources.v1 import hello
 from api_endpoint.resources.v1.face_recognition import router as face_recognition
+from api_endpoint.resources.v1.face_detection import router as face_detection
 
 api_routers = APIRouter()
 
+# TEST - FIELD: Test-API
 api_routers.include_router(hello.router, prefix="/helloworld", tags=["Greetings"])
+
+# MODEL - FIELD: FACE
 api_routers.include_router(face_recognition.router, prefix="/face-reco", tags=["ML-Model"])
+api_routers.include_router(face_detection.router, prefix="/face-det", tags=["ML-Model"])
