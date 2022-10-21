bb8d1ed1c17befb737568c6b6ba4bb452beb0dbd
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2022-10-21 20:03:07 +0700

    Add celery time helpers

--- celery_worker/app/helpers/time.py
+++ celery_worker/app/helpers/time.py
@@ -7,3 +7,6 @@ def now_utc():
     utc_now = utc_now.replace(tzinfo=pytz.utc)
     return utc_now
 
+
+def str_dd_mm_yyyy(time: datetime = now_utc(), space="-"):
+    return time.strftime("%d-%m-%Y")
